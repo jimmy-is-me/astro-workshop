4e56077d0779befd682e036388bd3e1b678d89cb
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-05-08 16:27:38 +0800

    fix: update site URL to match Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://astro-workshop.pages.dev',
+  site: 'https://astro-workshop-5ah.pages.dev',
   integrations: [mdx(), sitemap()],
   output: 'static',
+  trailingSlash: 'ignore',
 });
